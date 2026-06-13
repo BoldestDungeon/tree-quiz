244b6c60092672363aacf82daee26be71cb273c4
Ironskink (David Bilsky) <boldest.dungeon@gmail.com>
2026-06-13 16:30:24 -0500

    Create required files

---
+++ docs/src/treequiz.js
@@ -0,0 +1,29 @@
+function init(){}
+
+function loadLanguage(lang){
+  if(!lang){
+    lang = 'default';
+  }
+
+  fetch(`/data/language.${lang}.csv`)
+    .then(function(resp){ return resp.text() })
+    .then(parseLanguageCSV)
+}
+
+function parseLanguageCSV(responseText) {
+  const languageObj = {};
+
+  const languageArray=responseText.split('\n');
+
+  // Assume the first row is data headers that we do not need
+  // Assume file format is [key],[value]
+  for(let i=1; i<languageArray.length; i++) {
+    const row = languageArray[i];
+    if(!row.trim()) { continue; }
+    const rowArr = row.split(',');
+    const key = rowArr.splice(1).trim();
+    const value = (rowArr.join(',') || '').trim();
+    languageObj[key] = value;
+  }
+  return languageObj;
+}
