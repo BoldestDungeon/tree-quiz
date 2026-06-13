eefd0052dab8b3729e6f256fff519858b86acaf0
Ironskink (David Bilsky) <boldest.dungeon@gmail.com>
2026-06-13 17:07:23 -0500

    Add loading spinner

--- docs/src/treequiz.js
+++ docs/src/treequiz.js
@@ -7,7 +7,7 @@ function loadLanguage(lang){
     lang = 'default';
   }
 
-  fetch(`${baseURL}/data/language.${lang}.csv`)
+  return fetch(`${baseURL}/data/language.${lang}.csv`)
     .then(function(resp){ return resp.text() })
     .then(parseLanguageCSV)
 }
