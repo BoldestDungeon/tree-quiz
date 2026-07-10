0e7eba4bad8f12f8bf74409ba385d6b859216ce4
Ironskink (David Bilsky) <boldest.dungeon@gmail.com>
2026-07-09 19:09:55 -0500

    Fix img src URLs, Pt. 2

--- docs/src/treequiz.js
+++ docs/src/treequiz.js
@@ -380,7 +380,7 @@ function generateSeasonalQuestionHTML(question) {
 
     const answerImg = document.createElement('img');
     answerImg.className = 'answer_img seasonal_img';
-    answerImg.src = '/images/' + answerObj.image;
+    answerImg.src = baseURL + '/images/' + answerObj.image;
     answerEl.appendChild(answerImg);
 
     const answerText = document.createElement('span');
@@ -409,7 +409,7 @@ function generateMainQuestionHTML(question) {
 
   const questionImg = document.createElement('img');
   questionImg.className = 'question_img';
-  questionImg.src =  '/images/' + question.image;
+  questionImg.src =  baseURL + '/images/' + question.image;
   questionElement.appendChild(questionImg);
 
   const answersDiv = document.createElement('div');
